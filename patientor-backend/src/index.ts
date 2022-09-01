2c558016296acc5f4e6f437e0d233c55c5bb6042
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-09-01 18:20:33 -0500

    Exercise 9.10 completed

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,16 +1,18 @@
 import * as dotenv from 'dotenv'; 
 dotenv.config();
-import express from 'express'
-const app = express()
+import express from 'express';
+import diagnosesRouter from './routes/diagnosesRouter';
+const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT;
 
+app.use('/api/diagnoses' ,diagnosesRouter);
 
 app.get('/api/ping', (_req, res)=>{
-    res.send('Pong').end()
-})
+    res.send('Pong').end();
+});
 
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`);
