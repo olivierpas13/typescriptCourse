8f9349b9b391f79b186a50efac941cb65f1cf18d
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-09-01 10:38:59 -0500

    quickfix

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,4 +1,7 @@
 const calculateBmi = (height: number, weight: number) =>{
+
+    if(isNaN(height) || isNaN(weight)) throw new Error('Invalid arguments, not numbers')
+
     const result = weight / Math.pow(height/100, 2);
     if(result > 30)return 'Obese (Unhealthy weight)'
     if(result > 25)return 'Overweight (Unhealthy weight)'
